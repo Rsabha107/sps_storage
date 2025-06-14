36bfb28d728cff4b8d0d1ee496ebc6b5f6feac1f
Rsabha107 <61208207+Rsabha107@users.noreply.github.com>
2025-06-14 21:27:12 +0300

    settings and DB

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,13 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Models\GeneralSettings\Setting;
+use Illuminate\Validation\Rules\Password;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,5 +27,29 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+
+        // if (Schema::hasTable('settings')) {
+        //     $settings = Setting::all()->pluck('value', 'key')->toArray();
+        //     //  Log::info('Settings loaded from database', $settings);
+        //     config(['settings' => $settings]);  // Load the settings dynamically into Laravel's config
+        // }
+
+        if ($this->app->environment('azure')) {
+            URL::forceScheme('https');
+        }
+
+        Password::defaults(function () {
+            return Password::min(8)
+                ->mixedCase()
+                ->letters()
+                ->numbers()
+                ->symbols();
+        });
+
+
+        Carbon::setWeekendDays([
+            Carbon::FRIDAY,
+            Carbon::SATURDAY,
+        ]);
     }
 }
